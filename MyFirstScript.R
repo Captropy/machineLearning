6b4dbec378896ee68be4f3f3b9a86a5701ed6db0
pgifvars <per@gifvars.com>
2016-01-06 12:49:10 +0100

    Initialized the project. Added code to read data and split in to train and test set

--- MyFirstScript.R
+++ MyFirstScript.R
@@ -1,4 +1,22 @@
 # This is my new script for the machine learning course
 
-a <- 1
-b <- a*2
+#++++++++++++++++++++
+# Read data
+#++++++++++++++++++++
+
+pml.training <- read.csv("pml-training.csv")
+pml.testing <- read.csv("pml-testing.csv")
+
+#++++++++++++++++++++
+# Split training in test and cross-validation
+#++++++++++++++++++++
+
+## 75% of the sample size
+smp_size <- floor(0.75 * nrow(pml.training))
+
+## set the seed to make your partition reproductible
+set.seed(123)
+train_ind <- sample(seq_len(nrow(pml.training)), size = smp_size)
+
+train <- pml.training[train_ind, ]
+test <- pml.training[-train_ind, ]
